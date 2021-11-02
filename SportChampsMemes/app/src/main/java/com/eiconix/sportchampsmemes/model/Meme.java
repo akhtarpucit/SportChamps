0d79868026e9539f1c1ec87d1cb49fd03e5cc205
Akhtar Khan <akhtarpucit@gmail.com>
2021-11-02 16:08:44 +1100

    Implement memes response model

---
+++ SportChampsMemes/app/src/main/java/com/eiconix/sportchampsmemes/model/Meme.java
@@ -0,0 +1,85 @@
+package com.eiconix.sportchampsmemes.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Meme {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("width")
+    @Expose
+    private int width;
+    @SerializedName("height")
+    @Expose
+    private int height;
+    @SerializedName("box_count")
+    @Expose
+    private int boxCount;
+
+    private boolean isSelected;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getBoxCount() {
+        return boxCount;
+    }
+
+    public void setBoxCount(int boxCount) {
+        this.boxCount = boxCount;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+
+}

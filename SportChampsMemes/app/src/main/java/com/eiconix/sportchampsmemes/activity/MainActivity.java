51bdc0307d5c97c79b3793eb5f46aca233ce71d1
Akhtar Khan <akhtarpucit@gmail.com>
2021-11-02 15:49:35 +1100

    Initi project

---
+++ SportChampsMemes/app/src/main/java/com/eiconix/sportchampsmemes/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.eiconix.sportchampsmemes.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eiconix.sportchampsmemes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
